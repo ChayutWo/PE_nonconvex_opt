7df36f99eec5219219e19387aeb47e53b6f5cafa
Chayut Wongkamthong <chayutwo@gmail.com>
2021-06-05 20:21:20 +0700

    add constraint functions for other decline type

--- gradHess/computeExponentialConstr.m
+++ gradHess/computeExponentialConstr.m
@@ -1,43 +1,51 @@
-function [ f_har, g_har, B_har] = computeExponentialConstr( x, params )
+function [ f_exp, g_exp, B_exp] = computeExponentialConstr( x, params )
 %Compute function value, gradient, and Hessian of the Exponential constraint
 %q_i^(j)*deltat - (q_poti/d_i)(exp(-d_i*t_i^(j-1)) - exp(-d_i*t_i^(j))) = 0
 
+%parameters specific to wells with exponential decline
+n_well_type = params.n_expo;
+index = params.expo_index;
+
 %get parameters
-q_pot = params.q_pot;
+q_pot = params.q_pot(index);
+d = params.decline(index);
 deltaT = params.deltaT;
-d = params.decline;
 N = params.n_well;
 T = params.n_period;
-q_g = x(1:N*T);
-t_ij = x((N*T+1):2*N*T);
-t_ij_prev = x((N*T):2*N*T-1);
-for n = 1:N
+hyperbolic_const = params.hyperbolic_const;
+
+q_g = x(index); % q_g_i^(j)
+t_ij = x(N*T+index); % t_i^(j)
+t_ij_prev = x(N*T+index-1); % t_i^(j-1)
+for n = 1:n_well_type
     t_ij_prev(1+(n-1)*T) = 0;
 end
 
 %evaluate constraint function value
-f_har = deltaT*q_g - (q_pot./d).*log((1+d.*t_ij)./(1+d.*t_ij_prev));
+f_exp = deltaT*q_g - (q_pot./d).*(exp(-d.*t_ij_prev) - exp(-d.*t_ij));
 
 %calculate constraint gradient AT
-g_har = zeros(length(x),N*T);
+g_exp = zeros(length(x),n_well_type*T);
 %calculate hessian: 3NT+T x 3NT+T x NT
-B_har = zeros(length(x),length(x), N*T);
-for i = 1:N
+B_exp = zeros(length(x),length(x), n_well_type*T);
+
+start = min(index) - 1; %shift from wells with other types
+for i = 1:n_well_type
     for j = 1:T
         cur_index = (i-1)*T+j;
         q_pot_i = q_pot(cur_index);
         d_i = d(cur_index);
         t_cur = t_ij(cur_index);
-        g_har(cur_index,cur_index) = deltaT;
+        g_exp(start+cur_index,cur_index) = deltaT;
         if j == 1
-            g_har(N*T + cur_index,cur_index) = -q_pot_i/(1+d_i*t_cur);
-            B_har(N*T + cur_index,N*T +cur_index,cur_index) = q_pot_i*d_i/(1+d_i*t_cur)^2;
+            g_exp(N*T + start + cur_index,cur_index) = -q_pot_i*exp(-d_i*t_cur);
+            B_exp(N*T + start + cur_index,N*T +start + cur_index,cur_index) = q_pot_i*d_i*exp(-d_i*t_cur);
         else
             t_prev = t_ij(cur_index-1);
-            g_har(N*T + cur_index-1,cur_index) = q_pot_i/(1+d_i*t_prev);
-            g_har(N*T + cur_index,cur_index) = -q_pot_i/(1+d_i*t_cur);
-            B_har(N*T + cur_index-1,N*T +cur_index-1,cur_index) = -q_pot_i*d_i/(1+d_i*t_prev)^2;
-            B_har(N*T + cur_index,N*T +cur_index,cur_index) = q_pot_i*d_i/(1+d_i*t_cur)^2;
+            g_exp(N*T + start + cur_index-1,cur_index) = q_pot_i*exp(-d_i*t_prev);
+            g_exp(N*T + start + cur_index,cur_index) = -q_pot_i*exp(-d_i*t_cur);
+            B_exp(N*T + start + cur_index-1,N*T + start +cur_index-1,cur_index) = -q_pot_i*d_i*exp(-d_i*t_prev);
+            B_exp(N*T + start + cur_index,N*T +start + cur_index,cur_index) = q_pot_i*d_i*exp(-d_i*t_cur);
         end
     end
 end
--- gradHess/computeHyperbolicConstr.m
+++ gradHess/computeHyperbolicConstr.m
@@ -1,43 +1,52 @@
-function [ f_har, g_har, B_har] = computeHarmonicConstr( x, params )
-%Compute function value, gradient, and Hessian of the Harmonic constraint
-%q_i^(j)*deltat - (q_poti/d_i)ln((1+d_i*t_i^(j))/(1+d_i*t_i^(j-1))) = 0
+function [ f_hyp, g_hyp, B_hyp] = computeHyperbolicConstr( x, params )
+%Compute function value, gradient, and Hessian of the Hyperbolic constraint
+%q_i^(j)*deltat - (q_poti/(b-1)d_i)((1+b*d_i*t_i^(j))^(1-1/b)-(1+b*d_i*t_i^(j-1))^(1-1/b))
+
+%parameters specific to wells with hyperbolic decline
+n_well_type = params.n_hyper;
+index = params.hyper_index;
 
 %get parameters
-q_pot = params.q_pot;
+q_pot = params.q_pot(index);
+d = params.decline(index);
 deltaT = params.deltaT;
-d = params.decline;
 N = params.n_well;
 T = params.n_period;
-q_g = x(1:N*T);
-t_ij = x((N*T+1):2*N*T);
-t_ij_prev = x((N*T):2*N*T-1);
-for n = 1:N
+hyperbolic_const = params.hyperbolic_const;
+b = hyperbolic_const;
+
+q_g = x(index); % q_g_i^(j)
+t_ij = x(N*T+index); % t_i^(j)
+t_ij_prev = x(N*T+index-1); % t_i^(j-1)
+for n = 1:n_well_type
     t_ij_prev(1+(n-1)*T) = 0;
 end
 
 %evaluate constraint function value
-f_har = deltaT*q_g - (q_pot./d).*log((1+d.*t_ij)./(1+d.*t_ij_prev));
+f_hyp = deltaT*q_g - (q_pot./(d*(b-1))).*((1+b*d.*t_ij).^(1-1/b)-(1+b*d.*t_ij_prev).^(1-1/b));
 
 %calculate constraint gradient AT
-g_har = zeros(length(x),N*T);
+g_hyp = zeros(length(x),n_well_type*T);
 %calculate hessian: 3NT+T x 3NT+T x NT
-B_har = zeros(length(x),length(x), N*T);
-for i = 1:N
+B_hyp = zeros(length(x),length(x), n_well_type*T);
+
+start = min(index) - 1; %shift from wells with other types
+for i = 1:n_well_type
     for j = 1:T
         cur_index = (i-1)*T+j;
         q_pot_i = q_pot(cur_index);
         d_i = d(cur_index);
         t_cur = t_ij(cur_index);
-        g_har(cur_index,cur_index) = deltaT;
+        g_hyp(start+cur_index,cur_index) = deltaT;
         if j == 1
-            g_har(N*T + cur_index,cur_index) = -q_pot_i/(1+d_i*t_cur);
-            B_har(N*T + cur_index,N*T +cur_index,cur_index) = q_pot_i*d_i/(1+d_i*t_cur)^2;
+            g_hyp(N*T + start + cur_index,cur_index) = -q_pot_i*(1+b*d_i*t_cur)^(-1/b);
+            B_hyp(N*T + start + cur_index,N*T +start + cur_index,cur_index) = q_pot_i*d_i*(1+b*d_i*t_cur)^(-1-1/b);
         else
             t_prev = t_ij(cur_index-1);
-            g_har(N*T + cur_index-1,cur_index) = q_pot_i/(1+d_i*t_prev);
-            g_har(N*T + cur_index,cur_index) = -q_pot_i/(1+d_i*t_cur);
-            B_har(N*T + cur_index-1,N*T +cur_index-1,cur_index) = -q_pot_i*d_i/(1+d_i*t_prev)^2;
-            B_har(N*T + cur_index,N*T +cur_index,cur_index) = q_pot_i*d_i/(1+d_i*t_cur)^2;
+            g_hyp(N*T + start + cur_index-1,cur_index) = q_pot_i*(1+b*d_i*t_prev)^(-1/b);
+            g_hyp(N*T + start + cur_index,cur_index) = -q_pot_i*(1+b*d_i*t_cur)^(-1/b);
+            B_hyp(N*T + start + cur_index-1,N*T + start +cur_index-1,cur_index) = -q_pot_i*d_i*(1+b*d_i*t_prev)^(-1-1/b);
+            B_hyp(N*T + start + cur_index,N*T + start + cur_index,cur_index) = q_pot_i*d_i*(1+b*d_i*t_cur)^(-1-1/b);
         end
     end
 end
